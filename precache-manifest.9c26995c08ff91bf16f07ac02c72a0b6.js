ffe0a2a224b56e15fbc8896bbe8ac68e00fb1cde
Eric Groppe <emmgp@hotmail.com>
2020-07-27 21:34:10 -0300

    Updates

---
+++ precache-manifest.9c26995c08ff91bf16f07ac02c72a0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e717f0c8385aea3e3f2dffef23dbba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f851b9164e41ff098c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f82f83869c62934d47f",
+    "url": "/static/js/2.33983d0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33983d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f851b9164e41ff098c",
+    "url": "/static/js/main.840dfae5.chunk.js"
+  },
+  {
+    "revision": "3127fa9c9b5520db3630",
+    "url": "/static/js/runtime-main.b5e2c1b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
