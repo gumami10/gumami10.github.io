24a0bea8ae6e7af581afa89499d78ad83a183ce7
Eric Groppe <emmgp@hotmail.com>
2020-07-28 00:05:20 -0300

    commit build cause dont how to do it

---
+++ build/precache-manifest.59e88f9975842dfbf92258b6fc91bb67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632a588cfbe561ee358bc6a85f16783b",
+    "url": "/extremely-secret-project/index.html"
+  },
+  {
+    "revision": "ddba770c1a8f55d49fc3",
+    "url": "/extremely-secret-project/static/css/main.43d0d931.chunk.css"
+  },
+  {
+    "revision": "685b8f9ca42496f36437",
+    "url": "/extremely-secret-project/static/js/2.53dec89b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/extremely-secret-project/static/js/2.53dec89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddba770c1a8f55d49fc3",
+    "url": "/extremely-secret-project/static/js/main.acda2470.chunk.js"
+  },
+  {
+    "revision": "41078e23a832a0bfcd53",
+    "url": "/extremely-secret-project/static/js/runtime-main.1e2d7367.js"
+  },
+  {
+    "revision": "9c96749c79e60bf54dda5a378e5b42d2",
+    "url": "/extremely-secret-project/static/media/canvas.9c96749c.png"
+  }
+]);
